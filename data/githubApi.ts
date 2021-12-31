34fcd212c1ce4e2ee7966c8f9a9a346a71eab950
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-12-31 20:35:40 +0000

    Start working on GitHub API, add api_config to gitignore

---
+++ data/githubApi.ts
@@ -0,0 +1,2 @@
+const CONTENTS_URL =
+  "https://api.github.com/repos/dog-jamalam-tech/images/contents";
---
+++ data/types.ts
@@ -0,0 +1,5 @@
+interface Breed {
+  name: string;
+  subBreeds: string[];
+  images: string[];
+}
