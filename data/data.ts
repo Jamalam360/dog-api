d77b2dd2c803b7ab178f83e4ad10846221c63998
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2022-01-02 15:16:45 +0000

    Add Compile Workflow
    * Fix random false stopping GH API, add deno_compile workflow
    
    * Fix typos
    
    * Oops
    
    * Compile Workflow should now work
    
    * Fix artifact uploading
    
    * Run Compile after Format
    
    * Run Compile after Format

--- data/data.ts
+++ data/data.ts
@@ -16,7 +16,7 @@ const config = await getConfig();
 export async function updateData() {
   topLevelBreeds = [];
 
-  if (false && !(await isRateLimited())) {
+  if (!(await isRateLimited())) {
     const topLevelBreedDirectories = await getSubDirectories("");
 
     for (const topLevelBreedDirectory of topLevelBreedDirectories) {
